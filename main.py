e082196810d0c118d419e47f17adbb1d7f4871d0
LUIZ CARLOS MARTINS JUNIOR <104472961+ProfLuizCarlos@users.noreply.github.com>
2022-09-11 10:51:02 -0300

    Correcao dos verbos para ficar de acordo com o Sheldon. Dicionario de cores de fundo

--- main.py
+++ main.py
@@ -10,7 +10,20 @@ import random
 nomePlayer1 = input("Digite o seu nome: ")
 nomePlayer2 = 'Computador'
 
-elementos = {1: 'Pedra', 2: 'Papel', 3: 'Tesoura', 4: 'Lagarto', 5: 'Spock'}
+# o dicionario abaixo contem letra branca (30) com o fundo descrito
+cor_fundo = {'normal': '\033[m',
+             'vermelho': '\033[1;30;41m',
+             'verde': '\033[1;30;42m',
+             'azul': '\033[1;30;44m',
+             'roxo': '\033[1;30;45m',
+             'Azul claro': '\033[1;30;46m',
+             'cinza': '\033[1;30;47m'}
+elementos = {1: 'Pedra',
+             2: 'Papel',
+             3: 'Tesoura',
+             4: 'Lagarto',
+             5: 'Spock'}
+
 print('\033[0;97m*' * 20)
 for i in range(5):
     print(f'{i + 1} - {elementos[i + 1]}')
@@ -19,6 +32,7 @@ print('*' * 20)
 print()
 
 while True:
+    print()
     escolha = input('Digite a opção desejada: ')
     if (escolha.upper() == 'X'):
         print("Obrigado por jogar!")
@@ -26,10 +40,10 @@ while True:
     try:
         player1 = int(escolha)
     except:
-        print('\033[1;30;41m    Digite uma opção válida    \033[0;97m')
+        print(f"{cor_fundo['vermelho']}    Digite uma opção válida    {cor_fundo['normal']}")
         continue
     if (not 1 <= player1 <= 5):
-        print('\033[1;30;41m    Digite uma opção válida    \033[0;97m')
+        print(f"{cor_fundo['vermelho']}    Digite uma opção válida    {cor_fundo['normal']}")
         continue
     player2 = random.randint(1, 5)
 
@@ -37,16 +51,15 @@ while True:
     print(f"Opção de {nomePlayer1.title()}: {elementos[player1]}")
     print(f"Opção do {nomePlayer2}: {elementos[player2]}")
 
-    print()
     player1_ganha = {1: {3, 4}, 2: {1, 5}, 3: {2, 4}, 4: {2, 5}, 5: {1, 3}}
-    verbo1_ganha2 = {1: {3: ' quebra a ', 4: ' esmaga o '}, 2: {1: ' cobre a ', 5: ' refuta o '},
-                     3: {2: ' corta o ', 4: ' mata o '}, 4: {2: ' come o ', 5: ' envenena o '},
-                     5: {1: ' vaporiza a ', 3: ' mata o '}}
+    verbo1_ganha2 = {1: {3: ' amassa a ', 4: ' esmaga o '},
+                     2: {1: ' cobre a ', 5: ' refuta o '},
+                     3: {2: ' decapita o ', 4: ' mata o '},
+                     4: {2: ' come o ', 5: ' envenena o '},
+                     5: {1: ' vaporiza a ', 3: ' quebra o '}}
     if (player1 == player2):
-        print('\033[1;30;42m    EMPATE!!!    \033[0;97m')
+        print(f"{cor_fundo['verde']}    EMPATE!!!    {cor_fundo['normal']}")
     elif player2 in player1_ganha[player1]:
-        print(
-            f'\033[1;30;44m    {nomePlayer1.title()} ganhou!!!  {elementos[player1]}{verbo1_ganha2[player1][player2]}{elementos[player2]}     \033[0;97m')
+        print(f"{cor_fundo['azul']}    {nomePlayer1.title()} ganhou!!!  {elementos[player1]}{verbo1_ganha2[player1][player2]}{elementos[player2]}     {cor_fundo['normal']}")
     else:
-        print(
-            f'\033[1;30;45m    {nomePlayer2} ganhou!!!  {elementos[player2]}{verbo1_ganha2[player2][player1]}{elementos[player1]}    \033[0;97m')
+        print(f"{cor_fundo['roxo']}    {nomePlayer2} ganhou!!!  {elementos[player2]}{verbo1_ganha2[player2][player1]}{elementos[player1]}    {cor_fundo['normal']}")
